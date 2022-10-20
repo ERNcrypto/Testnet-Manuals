7b5abe7d132da82af44f0623437e3e43caed5680
stasiaantonova <109141998+stasiaantonova@users.noreply.github.com>
2022-10-21 01:36:04 +0200

    Update install_monitoring.sh

--- cosmos_node_monitoring/install_monitoring.sh
+++ cosmos_node_monitoring/install_monitoring.sh
@@ -57,5 +57,3 @@ echo -e "\e[1m\e[32m5. Downloading Node Monitoring config files ... \e[0m" && sl
 cd $HOME
 rm -rf cosmos_node_monitoring
 git clone https://github.com/ERNcrypto/Testnet-Manuals/tree/main/cosmos_node_monitoring.git
-
-chmod +x $HOME/cosmos_node_monitoring/add_validator.sh 
